e20dd89ae5cf9bd6fe442a78a1cb6153345ff5de
Heran3 <90075328+Heran3@users.noreply.github.com>
2021-11-11 21:51:55 -0500

    Add files via upload

---
+++ CheckingAccount.java
@@ -0,0 +1,37 @@
+public class CheckingAccount extends BankAccount {
+
+    //Declaring instance variables
+    private final static double fee=0.15;
+
+    //Parameterized constructor
+    public CheckingAccount(String name,double amount) {
+        super(name,amount);
+        super.setAccountNumber(getAccountNumber()+"-10");
+    }
+
+    public boolean withdraw(double amount)
+    {
+        boolean b;
+        if(amount>100 && getBalance()+fee>=amount)
+        {
+            super.withdraw(amount+fee);
+            System.out.printf("After withdrawl of $%.2f, balance = %.2f\n",amount,getBalance());
+            b=true;
+        }
+        else if(getBalance()>=amount)
+        {
+
+            super.withdraw(amount+fee);
+            System.out.printf("After withdrawl of $%.2f, balance = %.2f\n",amount,getBalance());
+            b=true;
+        }
+        else
+        {
+            b=false;
+
+        }
+        return b;
+    }
+
+
+}
